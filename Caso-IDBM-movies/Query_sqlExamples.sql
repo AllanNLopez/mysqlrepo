3692b4503f481e2ec00bc38a79d787eff06dded6
Allan N López <anlopez_1294@hotmail.com>
2024-04-23 19:59:03 -0600

    Funciones de agregado

--- Caso-IDBM-movies/Query_sqlExamples.sql
+++ Caso-IDBM-movies/Query_sqlExamples.sql
@@ -26,3 +26,55 @@ Select
 from recuperacion.movies t 
 where t.year = 2001
 order by rating desc ;
+
+/*
+    Funciones de agregado
+*/
+
+
+
+
+
+select 
+	y.director, count(y.title), sum(y.recaudacion) 
+from ( 
+		Select  
+			  t.director  
+			, t.title 
+			, t.rating 
+			, convert(
+				replace ( replace( t.gross ,'M', '') ,  '$', '' ),  decimal(12,2)
+			)    recaudacion  
+		from recuperacion.movies t 
+		where t.year > 1999
+) y group by y.director ;
+
+
+select count(*) t, count(distinct title) titulos, count(distinct director) directores from recuperacion.movies;
+------
+
+select
+	sub.director ,
+	count( sub.titulo ) titulos ,
+    sum( sub.recaudacion ) recaudacion , 
+    round( avg( sub.recaudacion ), 2 )recaudacion_promedio, 
+    avg(sub.rating) rating_promedio
+ from (
+		Select 
+			  t.title titulo
+			, t.year anio
+			, t.director
+			, t.certificate clasificacion
+			, t.runtime duracion 
+			, t.rating 
+			, convert(
+				replace (
+				replace( t.gross ,'M', '') ,  '$', '' ), 
+				decimal(12,2)
+			)    recaudacion  
+		from recuperacion.movies t 
+		where t.year between 1990 and 2000	 
+) sub 
+group by sub.director 
+order by avg( sub.recaudacion ) desc 
+;
