4414c478912f55367ef830c8b2fb7526294a9c3b
Allan N López <anlopez_1294@hotmail.com>
2025-02-25 23:58:45 -0600

    Add upd currency

--- Caso-FinancialSample/ddl-statements/sp_upd_currency.sql
+++ Caso-FinancialSample/ddl-statements/sp_upd_currency.sql
@@ -1,4 +1,9 @@
 
+/*
+	Actualizar la tasa solo si, el valor de tasa de cambio
+    es no nulo y es mayor que cero. De lo contrario no hacer cambios. 
+*/
+
 delimiter // 
 drop procedure sp_upd_currency; 
 
@@ -8,11 +13,13 @@ CREATE procedure sp_upd_currency(
     in p_currency_id int 
 )
 BEGIN	
-    UPDATE db_demo.currencies set exchange_rate = p_tasa 
-    where currency_id = p_currency_id;
-    commit;
+	IF p_tasa > 0 and p_tasa is not null THEN
+		UPDATE db_demo.currencies set exchange_rate = p_tasa  
+        where currency_id = p_currency_id;
+		commit;
+	ELSE
+		select "No se puede actualizar";
+	END IF;
 END;
 
-/* 1 USD = 149.86Y  */
-/*comentariop*/
 Call db_demo.sp_upd_currency(149.40, 9)
